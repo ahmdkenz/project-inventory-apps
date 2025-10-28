48d66cd551acd6ae2995d6f3f342871d1e22153f
Yuki <gigibengal14@gmail.com>
2025-10-28 16:36:22 +0700

    Update StaffNavigation

--- frontend/src/components/StaffNavigation.vue
+++ frontend/src/components/StaffNavigation.vue
@@ -23,16 +23,6 @@ const staffMenuItems: MenuItem[] = [
     icon: 'M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
   },
   {
-    path: '/staff/barang-masuk',
-    name: 'Barang Masuk',
-    icon: 'M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-  },
-  {
-    path: '/staff/barang-keluar',
-    name: 'Barang Keluar',
-    icon: 'M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-  },
-  {
     path: '/staff/laporan',
     name: 'Laporan',
     icon: 'M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z'
